8e266b1c16d74de82faf056fb2b41eb78670952d
Nolo <26887899+nolosb@users.noreply.github.com>
2023-02-24 16:49:59 +0000

    Initial commit

---
+++ javascripts/discourse/components/show-on-latest.js
@@ -0,0 +1,18 @@
+import Component from "@ember/component";
+import { inject as service } from "@ember/service";
+import discourseComputed from "discourse-common/utils/decorators";
+
+export default Component.extend({
+  tagName: "",
+  router: service(),
+
+  @discourseComputed("router.currentRouteName")
+  canDisplay(currentRouteName) {
+    //console.log(currentRouteName);
+    if (currentRouteName === `discovery.latest`) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+});
